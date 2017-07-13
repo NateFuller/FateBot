b99466970422d807eba731d8c594b031f394ea2e
Nathan Fuller <nfuller@brightcove.com>
2017-07-13 23:25:10 +0900

    Initial commit

---
+++ docs/tmi_info.js
@@ -0,0 +1,28 @@
+// Userstate
+// According to Twitch, the userstate object is always subject to change.
+var userstate = {
+    "badges": { "broadcaster": "1", "warcraft": "horde" },
+    "color": "#FFFFFF",
+    "display-name": "Schmoopiie",
+    "emotes": { "25": [ "0-4" ] },
+    "mod": true,
+    "room-id": "58355428",
+    "subscriber": false,
+    "turbo": true,
+    "user-id": "58355428",
+    "user-type": "mod",
+    "emotes-raw": "25:0-4",
+    "badges-raw": "broadcaster/1,warcraft/horde",
+    "username": "schmoopiie",
+    "message-type": "action"
+};
+
+// Roomstate
+// According to Twitch, the roomstate object is always subject to change.
+var roomstate = {
+    "broadcaster-lang": null,
+    "r9k": false,
+    "slow": false,
+    "subs-only": false,
+    "channel": "#fatenuller"
+};
